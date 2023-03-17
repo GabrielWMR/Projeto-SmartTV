e931dff46c48e1fa7354f6aa21196a80331c9e87
Gabriel <gabrielrodrigues_0@hotmail.com>
2023-03-17 10:48:42 -0300

    inclusao

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -1,5 +1,35 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class SmartTv {
+    boolean ligada=false;
+    int canal=1;
+    int volume=25;
+    
+    public void ligar(){
+        ligada=true;
+        System.out.println("TV ligada");
+
+    }
+    public void desligar(){
+        ligada=false;
+    }
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentar Volume " + volume);
+    }
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuir volume " + volume);
+    }
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+
+    }
+    public void aumentar(){
+        canal++;
+        System.out.println("o canal atual é: " + canal);
+    }
+
+    public void diminuir(){
+        canal--;
+        System.out.println("o canal atual é: " + canal);
     }
 }
